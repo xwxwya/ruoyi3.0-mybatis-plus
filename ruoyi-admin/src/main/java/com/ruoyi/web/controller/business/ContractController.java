161dd3d93c85555879e05d549f2d297978eea40c
wxj <841202690@qq.com>
2022-07-04 09:36:19 +0800

    example add

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/business/ContractController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/business/ContractController.java
@@ -1,9 +1,8 @@
 package com.ruoyi.web.controller.business;
 
 import com.ruoyi.business.entity.Contract;
-import com.ruoyi.business.mapper.ContractMapper;
+import com.ruoyi.business.service.ContractService;
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.system.mapper.SysConfigMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +16,25 @@ import java.util.List;
  * @date 2021/10/15 10:30
  */
 @RestController
-@RequestMapping("/business/contract")
+@RequestMapping("/contract")
 public class ContractController {
 
     @Resource
-    private ContractMapper mapper;
+    private ContractService service;
 
-    @Resource
-    private SysConfigMapper sysConfigMapper;
+//    @GetMapping("/get")
+////    @SerializationFilters(filters = {
+////            @SerializationFilter(target = BaseEntity.class,fields = {},exclusive = false),
+////            @SerializationFilter(target = SysUser.class,fields = {"nickName","userName"},exclusive = false)
+////    })
+//    public AjaxResult get(){
+//        List<Contract> all = service.getAll();
+//        return AjaxResult.success(all);
+//    }
 
-    @GetMapping("/get")
-    public AjaxResult get(){
-        List<Contract> all = mapper.getAll();
-        return AjaxResult.success(all);
+    @GetMapping("/example")
+    public AjaxResult example(){
+        List<Contract> res = service.example();
+        return AjaxResult.success(res);
     }
 }
